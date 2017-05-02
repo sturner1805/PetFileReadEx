46e188dad9cd10ab5294fc0e2443c7460d44103e
sturner1805 <sturner1805@gmail.com>
2017-05-02 10:01:29 +0100

    Add files via upload

---
+++ PetFileRead/src/Main.java
@@ -0,0 +1,79 @@
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+//edit edit edit edit
+
+public class Main {
+	
+	static ArrayList<Person> personList = new ArrayList<Person>();
+	static ArrayList<Animal> animalList = new ArrayList<Animal>();
+	
+	public static void main(String[]args){
+		
+		try{
+			File x = new File ("C:\\Users\\Sam\\Google Drive\\BAE\\NSAc\\Java\\ExData.txt");
+			Scanner sc = new Scanner(x);
+			String input  = "";
+			while(sc.hasNext()){
+				input += sc.nextLine() + "\n";
+			}
+			sc.close();		
+			
+			peopleExtractor(input);
+			animalExtractor(input);
+			for (Animal a : animalList){
+				System.out.println(a.getName());
+				System.out.println(a.getspecies());
+			}
+			personList.get(0);
+			
+
+		}catch(Exception e){
+		}
+	}
+
+	public static void peopleExtractor(String input){
+		String[] group = input.split("&");
+		String people = group[0];
+		
+		String[] person = people.split("%");
+		for(int i=0 ; i < person.length ; i++){
+			String personDetails = person[i];
+			String[] individual = personDetails.trim().split("\n");
+			String name = individual[0];
+			String dob = individual[1];
+			String job = individual[2];
+			String tempSalary = individual[3];
+			tempSalary = tempSalary.replace("£","").replace("Â", "");
+			int salary = Integer.parseInt(tempSalary);
+			Person p = new Person (name, dob, job, salary);
+			personList.add(p);
+		}
+	}
+	
+	public static void animalExtractor(String input){
+		String[] group = input.split("&");
+		String animals = group[1];
+		
+		String[] animal = animals.split("%");
+		for(int i=0 ; i < animal.length ; i++){
+			String animalDetails = animal[i];
+			String[] individual = animalDetails.trim().split("\n");
+			String name = individual[0];
+			String species = individual[1];
+			Animal a = new Animal (name, species);
+			animalList.add(a);
+		}
+	}
+	
+//	public static void petAssignment(String person, String pet){
+//		int i;
+//		int j;
+//		Person owner = personList.get(i);
+//		Animal owned = animalList.get(j);
+//		if (i == 0){
+//			j = 
+//		}
+//	}
+}
---
+++ PetFileRead/src/Person.java
@@ -0,0 +1,47 @@
+
+public class Person {
+
+	private String name;
+	private String dob;
+	private String job;
+	private int salary;
+	
+	Person(String name, String dob, String job, int salary) {
+		this.name = name;
+		this.dob = dob;
+		this.job = job;
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+}
